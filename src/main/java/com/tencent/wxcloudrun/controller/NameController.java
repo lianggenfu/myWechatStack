cf308614a3b2c77e98d9b70722d19a63b965549a
liangegnfu <917055347@qq.com>
2022-07-24 22:59:14 +0800

    结合前端h5页面

--- src/main/java/com/tencent/wxcloudrun/controller/NameController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/NameController.java
@@ -1,11 +1,14 @@
 package com.tencent.wxcloudrun.controller;
 
 import com.tencent.wxcloudrun.config.ApiResponse;
+import com.tencent.wxcloudrun.dto.NamerRequest;
 import com.tencent.wxcloudrun.service.NameGenerationService;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,14 +26,30 @@ public class NameController {
      * 获取姓名
      * @return API response json
      */
-    @GetMapping(value = "/api/getName")
-    ApiResponse get(String surName,int sexType,int count) {
-        logger.info("/api/count get request,surName:{},sexType:{},count:{}",surName,sexType,count);
-        //int sexType = 2; //1:男孩 2:女孩
-        List<String> names = nameGenerationService.getName(surName,sexType,count);
+    @PostMapping(value = "/api/getName")
+    ApiResponse get(@RequestBody NamerRequest request) {
+        logger.info("/api/count get request,surName:{},sexType:{},count:{}",request.getSurName(),request.getSexType(),request.getNumber());
+        String surName = request.getSurName();
+        String sexType = request.getSexType();
+        ApiResponse apiResponse = ApiResponse.error("");
+        if(StringUtils.isBlank(surName)){
+            apiResponse.setErrorMsg("姓不能为空!");
+            apiResponse.setCode(-120001);
+            return apiResponse;
+        }
+        if(StringUtils.isBlank(sexType)){
+            apiResponse.setErrorMsg("性别不能为空!");
+            apiResponse.setCode(-120002);
+            return apiResponse;
+        }else if(!sexType.equals("男") && !sexType.equals("女")){
+            apiResponse.setErrorMsg("性别输入不合法!");
+            apiResponse.setCode(-120003);
+            return apiResponse;
+        }
+        int sexTypeInt = sexType.equals("男")?1:2; //1:男孩 2:女孩
+        List<String> names = nameGenerationService.getName(surName,sexTypeInt,request.getNumber());
         for (int i = 0; i <names.size() ; i++) {
-            //List<String> names = nameGenerationService.getName(surName,sexType,i);
-            System.out.println("随机生成的第"+(i+1)+"个姓名为：" + names.get(i));
+            //System.out.println("随机生成的第"+(i+1)+"个姓名为：" + names.get(i));
         }
         //String name = "凉凉夜色";
         return ApiResponse.ok(names);
---
+++ src/main/java/com/tencent/wxcloudrun/dto/NamerRequest.java
@@ -0,0 +1,16 @@
+package com.tencent.wxcloudrun.dto;
+
+
+import lombok.Data;
+
+@Data
+public class NamerRequest {
+
+    private String surName;
+
+    private String sexType;
+
+    private int number;
+
+
+}
