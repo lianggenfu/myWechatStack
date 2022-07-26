dc8fda5178faae3796759acabf785615f9dfa9e0
liangegnfu <917055347@qq.com>
2022-07-26 23:47:37 +0800

    wechat demo test2->公众号回复测试

--- src/main/java/com/tencent/wxcloudrun/service/impl/WxServiceImpl.java
+++ src/main/java/com/tencent/wxcloudrun/service/impl/WxServiceImpl.java
@@ -1,10 +1,12 @@
 package com.tencent.wxcloudrun.service.impl;
 
 import com.tencent.wxcloudrun.model.TextMessage;
+import com.tencent.wxcloudrun.service.NameGenerationService;
 import com.tencent.wxcloudrun.service.WxService;
 import com.tencent.wxcloudrun.utils.XmlUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -17,6 +19,9 @@ import java.util.Map;
 public class WxServiceImpl implements WxService {
 
     final Logger logger = LoggerFactory.getLogger(WxServiceImpl.class);
+
+    @Autowired
+    private NameGenerationService nameGenerationService;
 
     @Override
     public void handleEvent(HttpServletRequest request, HttpServletResponse response) {
@@ -53,6 +58,38 @@ public class WxServiceImpl implements WxService {
     }
 
     private String handleEventSubscribe(Map<String, Object> map, String userOpenId) {
+        logger.info("---开始封装xml---:" + map.toString());
+        TextMessage textMessage = new TextMessage();
+        textMessage.setToUserName(map.get("FromUserName").toString());
+        textMessage.setFromUserName(map.get("ToUserName").toString());
+        textMessage.setCreateTime(System.currentTimeMillis());
+        textMessage.setMsgType("text");
+        textMessage.setContent(getContentByReq(map.get("Content").toString()));
+        return getXmlString(textMessage);
+    }
+
+    private String getContentByReq(String content) {
+        if("取名".equals(content)){
+            return "请输入姓氏,性别(逗号隔开)";
+        }
+        if(content.contains(",")){
+            String[] strs = content.split(",");
+            if(strs[1].equals("男")){
+                return nameGenerationService.getName(strs[0],1,1).get(0);
+            }else if(strs[1].equals("女")){
+                return nameGenerationService.getName(strs[0],2,1).get(0);
+            }else{
+                return "格式异常！\n" +
+                        "eg: 梁,女";
+            }
+        }
+        if("取名内测链接".equals(content)){
+            return "取名链接: "+ "https://springboot-jhx8-1412-4-1313045077.sh.run.tcloudbase.com/index.html";
+        }
+        return "欢迎关注eather_liang!";
+    }
+
+    private String handleEventSubscribe2(Map<String, Object> map, String userOpenId) {
         logger.info("---开始封装xml---:" + map.toString());
         TextMessage textMessage = new TextMessage();
         textMessage.setToUserName(map.get("FromUserName").toString());
